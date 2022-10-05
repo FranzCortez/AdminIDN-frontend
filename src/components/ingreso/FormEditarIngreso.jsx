b43bc156bdf5b6c683d1b56afd6b2e0a0d84a652
FranzCortez <franzco2000@gmail.com>
2022-10-05 11:19:16 -0300

    feat(ingreso): editar ingreso

---
+++ src/components/ingreso/FormEditarIngreso.jsx
@@ -0,0 +1,380 @@
+import React, { Fragment, useState, useEffect, useContext } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { IoArrowBackCircleOutline } from "react-icons/io5";
+import { AiOutlineTool } from "react-icons/ai";
+import { FiEdit } from "react-icons/fi";
+import Swal from 'sweetalert2';
+
+import { CRMContext } from '../context/CRMContext';
+import clienteAxios from '../../config/axios';
+
+function FormEditarIngreso() {
+
+    const { id } = useParams();
+
+    const [ ingreso, guardarIngreso ] = useState({
+        nombre: '',
+        marca: '',
+        fecha: '',
+        modelo: '',
+        comentario: '',
+        numeroInterno: '',
+        numeroGuiaCliente: '',
+        numeroSerie: '',
+        clienteContactoId: '',
+        tipoHerramientaId: 0
+    });
+    const [ tipos, guardarTipos ] = useState([]);
+    const [ tipo, guardarTipo ] = useState(0);
+    const [ empresas, guardarEmpresas ] = useState([]);
+    const [ empresa, guardarEmpresa ] = useState(0);
+    const [ contactos, guardarContactos ] = useState([]);
+    const [ contacto, guardarContacto ] = useState(0);
+    const [ fecha, guardarFecha ] = useState('');
+
+    let idEmpresa = 0;
+
+    // usar context
+    const [auth, guardarAuth] = useContext(CRMContext);
+
+    let navigate = useNavigate();
+    
+    const actualizarState = e => {
+        
+        guardarIngreso({
+            ...ingreso,
+            [e.target.name] : e.target.value
+        });
+
+        if(e.target.name === 'fecha'){
+            guardarFecha(e.target.value)
+        }      
+    }
+
+    const editarIngreso = async (e) => {
+        e.preventDefault();
+
+        try {            
+            const res = await clienteAxios.put(`/ih/ingreso/${id}`, ingreso,{
+                headers: {
+                    Authorization: `Bearer ${auth.token}`
+                }
+            });
+
+            Swal.fire({
+                title: 'Se actulizo correctamente el ingreso',
+                text: res.data.msg,
+                type: 'success',
+                timer: 3500
+            });
+                
+            // redireccionar
+            navigate('/ingresos', {replace: true});
+        } catch (error) {
+            console.log(error)
+            Swal.fire({
+                type: 'error',
+                title: 'Hubo un error',
+                text: error.response.data.msg,
+                timer: 1500
+            });            
+        }
+        
+    }
+
+    const consultarAPI = async () => {
+        try {
+            const res = await clienteAxios.get(`ih/ingreso/${id}`,{
+                headers: {
+                    Authorization: `Bearer ${auth.token}`
+                }
+            });
+            
+            guardarIngreso({
+                nombre: res.data.nombre,
+                marca: res.data.marca,
+                fecha: res.data.fecha,
+                modelo: res.data.modelo,
+                comentario: res.data.comentario,
+                numeroInterno: res.data.numeroInterno,
+                numeroGuiaCliente: res.data.numeroGuiaCliente,
+                numeroSerie: res.data.numeroSerie,
+                clienteContactoId: res.data.clienteContactoId,
+                tipoHerramientaId: res.data.tipoHerramientaId
+                
+            });
+            guardarFecha(res.data.fecha);
+
+            const resTipo = await clienteAxios.get(`tipo/categoria/herramienta`, {
+                headers: {
+                    Authorization: `Bearer ${auth.token}`
+                }
+            });
+            
+            guardarTipo(res.data.tipoHerramientaId);
+            guardarTipos(resTipo.data);
+
+            const resEmpresa = await clienteAxios.get(`empresas/empresaNombre`, {
+                headers: {
+                    Authorization: `Bearer ${auth.token}`
+                }
+            });
+            
+            guardarEmpresas(resEmpresa.data);
+            guardarEmpresa(res.data.clienteContacto.clienteEmpresa.id);
+
+            idEmpresa = res.data.clienteContacto.clienteEmpresa.id;
+
+            const resContacto = await clienteAxios.get(`contactos/contacto/${idEmpresa}`, {
+                headers: {
+                    Authorization: `Bearer ${auth.token}`
+                }
+            });
+            
+            guardarContactos(resContacto.data);
+            guardarContacto(res.data.clienteContactoId);
+
+
+        } catch (error) {
+            console.log(error)
+            if(error.request.status === 404 ) {
+                Swal.fire({
+                    type: 'error',
+                    title: 'Hubo un error',
+                    text: error.response.data.msg,
+                    timer: 1500
+                })
+            }
+            // redireccionar
+            navigate('/ingresos', {replace: true});
+        }
+    }
+
+    const consultarContactos = async (e) => {
+        try {
+            idEmpresa = (e.target.value);
+            guardarEmpresa(e.target.value);
+
+            const res = await clienteAxios.get(`contactos/contacto/${idEmpresa}`, {
+                headers: {
+                    Authorization: `Bearer ${auth.token}`
+                }
+            });
+            
+            guardarContactos(res.data);
+        } catch (error) {
+            if(error.request.status === 404 ) {
+                Swal.fire({
+                    type: 'error',
+                    title: 'Hubo un error',
+                    text: error.response.data.msg,
+                    timer: 1500
+                })
+            }
+            // redireccionar
+            navigate('/ingresos', {replace: true});
+        }        
+    }
+
+    const validarForm = () => {
+
+        const { nombre, marca, fecha, modelo, comentario, clienteContactoId, tipoHerramientaId } = ingreso;
+
+
+        if( !(!nombre.length || !marca.length || !fecha.length || !modelo.length || !clienteContactoId || !comentario.length || !tipoHerramientaId) ){
+            return false;
+        }
+
+        return true;
+    }
+
+    const contactoListo = (e) => {
+        guardarIngreso({
+            ...ingreso,
+            "clienteContactoId" : e.target.value
+        });
+        guardarContacto(e.target.value);
+    }
+
+    const tipoListo = (e) => {
+        guardarIngreso({
+            ...ingreso,
+            "tipoHerramientaId" : e.target.value
+        });
+
+        guardarTipo(e.target.value);
+    }
+
+    useEffect(() => {
+        if(!(auth.auth && (localStorage.getItem('token') === auth.token))){  
+            navigate('/login', {replace: true});
+        } else if (auth.tipo !== 1 && auth.tipo !== 2){ 
+            navigate('/login', {replace: true});
+        } else {
+            consultarAPI();
+        }
+    },[]);
+
+    return (
+        <Fragment>
+            <div className="card contenedor">
+                <div className="card-header">
+                    <FiEdit size={50} color={"#333333"}/>
+                    <AiOutlineTool size={50} color={"#333333"}/>
+                    <h1>Editar Ingreso</h1>
+                </div>
+                <div className="card-body">
+
+                    <div className='top-left'>
+                        <Link to={'/ingresos'} className="btn-new btn-return"><IoArrowBackCircleOutline size={25}/> Regresar</Link>
+                    </div>
+
+                    <h2 className='card-body-subtitle'> Modifique los campos según corresponda: </h2>
+                    <p className='text-center'>Asegúrese que están ingresados anteriormente los primeros 3 datos para poder seleccionarlos</p>
+
+                    <form onSubmit={editarIngreso}>
+                        <div className='campo'>
+                            <label htmlFor="nombre">Seleccione Empresa:</label>
+                            <select name="empresa" id="empresa"  value={empresa} onChange={consultarContactos}>
+                                <option value={0} disabled> -- Seleccione -- </option>    
+                                {
+                                    empresas.map(empresa => (
+                                        <option value={empresa.id} key={empresa.id}>{empresa.nombre}</option>
+                                    ))
+                                }
+                            </select>     
+                            <Link to={'/clientes/nuevo'} className="btn-new btn-success-new">Crear Nueva Empresa</Link>                    
+                        </div>                                   
+
+                        <div className='campo'>
+                            <label htmlFor="nombre">Seleccione Contacto Empresa:</label>
+                            <select name="clienteContactoId" id="clienteContactoId"  value={contacto} onChange={contactoListo}>
+                                <option value={0} disabled> -- Seleccione -- </option>    
+                                {
+                                    contactos.map(contacto => (
+                                        <option value={contacto.id} key={contacto.id}>{contacto.nombre}</option>
+                                    ))
+                                }
+                            </select>   
+                            <Link to={`/clientes/contacto/${empresa}/nuevo`} className="btn-new btn-success-new">Crear Nueva Contacto</Link>                         
+                        </div>                     
+ 
+                        <div className='campo'>
+                            <label htmlFor="nombre">Seleccione Tipo Herramienta:</label>
+                            <select name="empresa" id="empresa"  value={tipo} onChange={tipoListo}>
+                                <option value={0} disabled> -- Seleccione -- </option>    
+                                {
+                                    tipos.map(tipo => (
+                                        <option value={tipo.id} key={tipo.id}>{tipo.nombre}</option>
+                                    ))
+                                }
+                            </select>     
+                            <Link to={'#'} className="btn-new btn-success-new">Crear Tipo Herramienta</Link>                    
+                        </div>                
+
+                        <div className='campo'>
+                            <label htmlFor="fecha">Fecha Ingreso:</label>
+                            <input 
+                                type="date" 
+                                id='fecha'
+                                name='fecha'
+                                value={fecha}
+                                placeholder='Fecha Ingreso de la Herramienta'
+                                onChange={actualizarState}
+                            />
+                        </div>
+
+                        <div className='campo'>
+                            <label htmlFor="nombre">Nombre:</label>
+                            <input 
+                                type="text" 
+                                id='nombre'
+                                name='nombre'
+                                value={ingreso.nombre}
+                                placeholder='Nombre de la Herramienta'
+                                onChange={actualizarState}
+                            />
+                        </div>
+
+                        <div className='campo'>
+                            <label htmlFor="marca">Marca:</label>
+                            <input 
+                                type="text" 
+                                id='marca'
+                                name='marca'
+                                value={ingreso.marca}
+                                placeholder='Marca de la Herramienta'
+                                onChange={actualizarState}
+                            />
+                        </div>
+                        
+                        <div className='campo'>
+                            <label htmlFor="modelo">Modelo:</label>
+                            <input 
+                                type="text" 
+                                id='modelo'
+                                name='modelo'
+                                value={ingreso.modelo}
+                                placeholder='Modelo de la Herramienta'
+                                onChange={actualizarState}
+                            />
+                        </div>
+                        
+                        <div className='campo'>
+                            <label htmlFor="numeroSerie">N° Serie:</label>
+                            <input 
+                                type="number" 
+                                id='numeroSerie'
+                                name='numeroSerie'
+                                value={ingreso.numeroSerie}
+                                placeholder='N° Serie de la Herramienta'
+                                onChange={actualizarState}
+                            />
+                        </div>
+                        
+                        <div className='campo'>
+                            <label htmlFor="numeroInterno">N° Interno:</label>
+                            <input 
+                                type="number" 
+                                id='numeroInterno'
+                                name='numeroInterno'
+                                value={ingreso.numeroInterno}
+                                placeholder='N° Interno de la Herramienta'
+                                onChange={actualizarState}
+                            />
+                        </div>
+                        
+                        <div className='campo'>
+                            <label htmlFor="numeroGuiaCliente">N° Guía Cliente:</label>
+                            <input 
+                                type="number" 
+                                id='numeroGuiaCliente'
+                                name='numeroGuiaCliente'
+                                value={ingreso.numeroGuiaCliente}
+                                placeholder='N° Guía Cliente de la Herramienta'
+                                onChange={actualizarState}
+                            />
+                        </div>
+
+                        <div className='campo'>
+                            <label htmlFor="comentario">Comentario:</label>
+
+                            <textarea name="comentario" id="comentario" cols="50" rows="10" defaultValue={ingreso.comentario} onChange={actualizarState} ></textarea>
+                        </div>
+ 
+                        <div className="enviar">
+                            <input 
+                                type="submit" 
+                                className={ validarForm() ? "btn-new"  : 'btn-new btn-success-new'}
+                                value="Guardar Cambios"
+                                disabled={validarForm()}
+                            />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </Fragment>
+    )
+}
+
+export default FormEditarIngreso;
