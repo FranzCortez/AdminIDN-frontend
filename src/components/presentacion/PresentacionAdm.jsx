f728a92368b23b1d2040a9c6ec46ae2ddf43640a
FranzCortez <franzco2000@gmail.com>
2022-12-08 13:00:57 -0300

    feat(presentaciones): nuevos tipos de display

--- src/components/presentacion/PresentacionAdm.jsx
+++ src/components/presentacion/PresentacionAdm.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css'
+import './presentacion.css'
 
 function PresentacionAdm() {
+    
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
     return (
         <div>
-            <embed 
-                className='presentacion' 
-                src='presentacion/PRESENTACION IMPACTO DEL NORTE ADMINISTRACION.pdf' 
-                type="application/pdf" 
-                title='PRESENTACION IMPACTO DEL NORTE GERENTE'
-                frameborder="0" 
-                allowfullscreen
-            />    
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.1.81/build/pdf.worker.min.js">
+                <Viewer fileUrl={'presentacion/PRESENTACION IMPACTO DEL NORTE ADMINISTRACION.pdf'} plugins={[defaultLayoutPluginInstance]} ></Viewer>
+            </Worker>
         </div>
     )
-    }
+}
 
 export default PresentacionAdm
--- src/components/presentacion/PresentacionVenta.jsx
+++ src/components/presentacion/PresentacionVenta.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css'
+import './presentacion.css'
 
 function PresentacionVenta() {
+
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+    
     return (
         <div>
-            <embed 
-                className='presentacion' 
-                src='presentacion/PRESENTACION IMPACTO DEL NORTE VENTAS.pdf' 
-                type="application/pdf" 
-                title='PRESENTACION IMPACTO DEL NORTE GERENTE'
-                frameborder="0" 
-                allowfullscreen
-            />    
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.1.81/build/pdf.worker.min.js">
+                <Viewer fileUrl={'presentacion/PRESENTACION IMPACTO DEL NORTE VENTAS.pdf'} plugins={[defaultLayoutPluginInstance]} ></Viewer>
+            </Worker>
         </div>
     )
 }
