5722525a1f477844831d0877335eacf6e86425c9
FranzCortez <franzco2000@gmail.com>
2023-03-30 13:44:39 -0300

    balance: componente tabla

---
+++ src/components/balance/componenteBalance/TablaBalance.jsx
@@ -0,0 +1,46 @@
+import React, { Fragment } from 'react';
+import moment from 'moment';
+
+function TablaBalance({ data, total }) {
+
+    const valorNumero = (numero) => new Intl.NumberFormat().format(numero);
+
+    return (
+        <div className="table-responsive balance">
+            <table className="table table-hover">
+                <thead>
+                    <tr className='table__head'>
+                        <th scope="col">OTIN</th>
+                        <th scope="col">Cliente</th>
+                        <th scope="col">N° Factura</th>
+                        <th scope="col">Fecha Factura</th>
+                        <th scope="col">Monto</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.length > 0 ?
+                            data.map((info, index) => (
+                                <tr key={index}>
+                                    <td>{info?.otines}</td>
+                                    <td>{info?.herramientas[0].clienteContacto.clienteEmpresa.nombre}</td>
+                                    <td>{info?.numeroFactura}</td>
+                                    <td>{moment(info?.fechaFactura).format('DD-MM-YYYY')}</td>
+                                    <td>$ {valorNumero(info?.monto)}</td>
+                                </tr>
+                            ))
+                        :
+                            null
+                    }
+
+                    <tr className='table__tr boleta__total'>
+                        <td colSpan={4} ><span className='boleta__valor-total' >TOTAL:</span></td>
+                        <td><span className='boleta__valor-total'>${valorNumero(total)}</span></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default TablaBalance
