69ef8cee048da9f4aad4fdc2f4b881c3c1f2ecd5
FranzCortez <franzco2000@gmail.com>
2023-03-30 16:19:46 -0300

    feat(grafico): comparacion de dos meses o años

---
+++ src/components/balance/componenteBalance/GraficoBarra.jsx
@@ -0,0 +1,40 @@
+import { Fragment } from "react";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+
+function GraficoBarra({ totalPrimero, totalSegundo, texto, uv, pv }) {
+    const data = [
+        {
+          name: texto,
+          [uv] : totalPrimero,
+          [pv] : totalSegundo,
+          amt: totalSegundo
+        },
+    ];
+
+
+    return (
+        <div style={{ width: '100%', height: 300 }}>
+            <ResponsiveContainer width="90%" height="100%">
+                
+                <BarChart
+                
+                    width={500}
+                    height={700}
+                    data={data}
+                    barCategoryGap={'5%'}
+                    margin={{top: 20, right: 20, bottom: 20, left: 50}}
+                >
+                    <CartesianGrid strokeDasharray="1 1" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip/>
+                    <Legend verticalAlign="top" align="center" />
+                    <Bar dataKey={`${pv}`} fill="#8884d8" />
+                    <Bar dataKey={`${uv}`} fill="#82ca9d" />
+                </BarChart>
+            </ResponsiveContainer>
+        </div>
+    )
+}
+
+export default GraficoBarra;
