489508dac7a07093af2ef58fc1139ddd6ad803d4
FranzCortez <franzco2000@gmail.com>
2023-04-04 12:40:29 -0400

    feat(balance): tabla resumen facturacion anual

---
+++ src/components/balance/componenteBalance/TablaResumen.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Fragment } from 'react';
+
+function TablaResumen({ meses, data, año, total }) {
+
+    const valorNumero = (numero) => new Intl.NumberFormat().format(numero);
+
+    return (
+        <Fragment>
+
+            <h2 className='card-body-subtitle'>
+                Facturación año {año} 
+            </h2>
+
+            <div className="table-responsive">
+                <table className="table table-hover">
+                    <thead>
+                        <tr className='table__head'>
+                            {
+                                meses.length > 0 ?
+                                    meses.map((mes,index) =>(
+                                        <th scope="col" key={index}>{mes}</th>
+                                    ))
+                                :
+                                null
+                            }
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        {
+                            data.length > 0 ?
+                                    data.map((info, index) => (
+                                        <td  key={index} style={{textAlign:'center'}} >$ {valorNumero(info)}</td>
+                                    ))
+                                    :
+                                    null
+                                }
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <h3 className='card-body-subtitle'>
+                Facturación total: $ {valorNumero(total)}
+            </h3>
+        </Fragment>
+    )
+}
+
+export default TablaResumen;
