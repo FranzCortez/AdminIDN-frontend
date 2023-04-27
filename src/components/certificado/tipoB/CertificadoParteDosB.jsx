0a726c332585c718c99a2150b7d8e380508f4085
FranzCortez <franzco2000@gmail.com>
2023-04-27 12:25:22 -0400

    fix(certificado): cambio de , por .

--- src/components/certificado/tipoB/CertificadoParteDosB.jsx
+++ src/components/certificado/tipoB/CertificadoParteDosB.jsx
@@ -94,7 +94,9 @@ function CertificadoParteDosB({ onButtonClick, guardarDatosSegundo, rango, unida
 
             const actualizar = tabla.comparcion;
 
-            actualizar[existe.name].numero = parseFloat(e.target.value);
+            const numero = e.target.value.split(",").join(".");
+
+            actualizar[existe.name].numero = parseFloat(numero);
 
             if ( !actualizar[existe.name].numero ) {
                 actualizar[existe.name].usado = false;
