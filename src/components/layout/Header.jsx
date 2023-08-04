211aa6fdb6bc3bc624c972a4c20a027f3a070497
FranzCortez <franzco2000@gmail.com>
2023-08-03 21:51:21 -0400

    feat(com):cambio de menu a comercializadora

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, Fragment } from "react";
+import React, { useContext, Fragment, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 import { VscTools } from 'react-icons/vsc';
@@ -7,7 +7,7 @@ import { AiOutlineDollarCircle, AiOutlinePoweroff, AiOutlineTool, AiFillHome } f
 import { ImQrcode } from "react-icons/im";
 import { FiUsers } from "react-icons/fi";
 import { GoGraph } from "react-icons/go";
-import { BsCalendarCheck } from "react-icons/bs";
+import { BsCalendarCheck, BsShopWindow } from "react-icons/bs";
 
 import Sidebar from "./Sidebar";
 
@@ -16,10 +16,23 @@ import { CRMContext } from "../context/CRMContext";
 function Header() {
 
     const [auth, guardarAuth] = useContext(CRMContext);
+    const [lugar, guardarLugar] = useState(true);
 
     let navigate = useNavigate();
     
     if(!auth.auth) return null;
+
+    const EIRL = {
+        ruta: '/home',
+        texto: 'EIRL',
+        icono: <AiOutlineTool size={50} color={"#333333"} />
+    }
+
+    const comercializadora = {
+        ruta: '/comercializadora',
+        texto: 'Comercializadora',
+        icono: <BsShopWindow size={50} color={"#333333"} />
+    }
 
     const cerrarSesion = () => {
         guardarAuth({
@@ -65,6 +78,33 @@ function Header() {
         } 
     ]
 
+    let rutaComercializadora = [
+        {
+            ruta: '/home',
+            texto: 'Inicio',
+            icono: <AiFillHome size={50} color={"#333333"}/>
+        },
+        {
+            ruta: '/clientes',
+            texto: 'Clientes',
+            icono: <RiContactsBook2Line size={50} color={"#333333"}/>
+        },
+        {
+            ruta: '/ingresos',
+            texto: 'Ingresos',
+            icono: <AiOutlineTool size={50} color={"#333333"}/>
+        },
+        {
+            ruta: '/ingresos/tipoherramienta',
+            texto: 'Tipo Herramienta',
+            icono: <VscTools size={50} color={"#333333"}/>
+        },
+        {
+            ruta: '/qr/sd/form',
+            texto: 'QR sin Datos',
+            icono: <ImQrcode size={50} color={"#333333"}/>
+        } 
+    ]
     if(auth.tipo === 1) {
         rutas.push(
             {
@@ -88,6 +128,50 @@ function Header() {
                 icono: < BsCalendarCheck size={50} color={"#333333"} />
             }
         )
+
+        rutaComercializadora.push(
+            {
+                ruta: '/facturas',
+                texto: 'Facturas',
+                icono: <AiOutlineDollarCircle size={50} color={"#333333"}/>
+            },  
+            {
+                ruta: '/balance',
+                texto: 'Balance',
+                icono: <GoGraph size={50} color={"#333333"}/>
+            },
+            {
+                ruta: '/usuarios',
+                texto: 'Usuarios',
+                icono: <FiUsers size={50} color={"#333333"}/>
+            },
+            {
+                ruta: '/checklist',
+                texto: 'Solicitudes',
+                icono: < BsCalendarCheck size={50} color={"#333333"} />
+            }
+        )
+    }
+
+    const cambio = () => {
+        
+        const sidebar = document.querySelector('.sidebar__color');
+        const menu = document.querySelector('.sidebar__header');
+        const header = document.querySelector('.header');
+        const canvas = document.querySelector('.offcanvas-header');
+        if ( lugar ) {
+            sidebar.style.backgroundColor = '#2566e8'
+            menu.style.backgroundColor = '#2566e8'
+            canvas.style.backgroundColor = '#2566e8'
+            header.style.backgroundColor = '#2566e8'
+        } else {
+            sidebar.style.backgroundColor = '#ff8533'
+            header.style.backgroundColor = '#ff8533'
+            canvas.style.backgroundColor = '#ff8533'
+            menu.style.backgroundColor = '#ff8533'
+        }
+
+        guardarLugar(!lugar);
     }
 
     return (
@@ -115,9 +199,57 @@ function Header() {
                 <div className="offcanvas-body">
                     <div className="sidebar__cuerpo">
                        { // controlador a nuevos componente
-                            rutas.map((ruta, index) => (
-                                <Sidebar ruta={ruta} key={index} />
-                            ))
+                            auth.tipo === 1 ?
+
+                                lugar ?
+
+                                <Fragment>
+
+                                    <button onClick={cambio} className="sidebar__none">
+
+                                        <Link to={comercializadora.ruta}>
+                                            <div className="sidebar__ruta" data-bs-dismiss="offcanvas" >
+                                                {comercializadora.icono}
+                                                <h2 className="sidebar__text">{comercializadora.texto}</h2>
+                                            </div>
+                                        </Link>
+
+                                    </button>
+
+                                    {
+                                        rutas.map((ruta, index) => (
+                                            <Sidebar ruta={ruta} key={index} />
+                                        ))
+                                    }
+                                </Fragment>
+
+                                :
+
+                                <Fragment>
+
+                                    <button onClick={cambio} className="sidebar__none">
+
+                                        <Link to={EIRL.ruta}>
+                                            <div className="sidebar__ruta" data-bs-dismiss="offcanvas" >
+                                                {EIRL.icono}
+                                                <h2 className="sidebar__text">{EIRL.texto}</h2>
+                                            </div>
+                                        </Link>
+
+                                    </button>
+
+                                    {
+                                        rutaComercializadora.map((ruta, index) => (
+                                            <Sidebar ruta={ruta} key={index} />
+                                        ))
+                                    }
+                                </Fragment>
+
+                            :
+
+                                rutas.map((ruta, index) => (
+                                    <Sidebar ruta={ruta} key={index} />
+                                ))
                        }
                        <div className="sidebar__ruta" data-bs-dismiss="offcanvas" onClick={cerrarSesion}>
                             <AiOutlinePoweroff size={50} color={"#333333"}/>
--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Sidebar({ruta}) {
+
     return (
         <Link to={ruta.ruta}>
-            <div className="sidebar__ruta" data-bs-dismiss="offcanvas">
+            <div className="sidebar__ruta" data-bs-dismiss="offcanvas" >
                 {ruta.icono}
                 <h2 className="sidebar__text">{ruta.texto}</h2>
             </div>
